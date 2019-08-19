bd9f823b4a46e71acfd9ca1cadf187697267d3f0
hauchenglee <hauchenglee@gmail.com>
2019-08-19 08:04:29 +0800

    add max pq

--- src/stacks_and_queues/Main.java
+++ src/stacks_and_queues/Main.java
@@ -5,7 +5,7 @@ import edu.princeton.cs.algs4.StdOut;
 
 public class Main {
     public static void main(String[] args) {
-        Stack stack = new Stack();
+        Stack<String> stack = new Stack<>();
         while (!StdIn.isEmpty()) {
             String s = StdIn.readString();
             if (s.equals("-")) StdOut.print(stack.pop());
---
+++ src/stacks_and_queues/MaxPQ.java
@@ -0,0 +1,66 @@
+package stacks_and_queues;
+
+import util.SortingAbstractions;
+
+// APIs and Elementary Implementations
+// PriorityQueue
+public class MaxPQ<Key extends Comparable<Key>> {
+    private Key[] pq;
+    private int N;
+
+    MaxPQ(int capacity) {
+        pq = (Key[]) new  Comparable[capacity + 1];
+    }
+
+    void insert(Key x) {
+        pq[++N] = x;
+        swim(N);
+    }
+
+    Key delMax() {
+        Key max = pq[1];
+        exch(1, N--);
+        sink(1);
+        pq[N + 1] = null; // prevent loitering
+        return max;
+    }
+
+    boolean isEmpty() {
+        return N == 0;
+    }
+
+    Key max() {
+        return null;
+    }
+
+    int size() {
+        return -1;
+    }
+
+    private void swim(int k) {
+        while (k > 1 && less(k / 2, k)) {
+            exch(k, k / 2);
+            k = k / 2;
+        }
+    }
+
+    private void sink(int k) {
+        while (2 * k <= N) {
+            int j = 2 * k;
+            if (j < N && less(j, j + 1)) j++;
+            if (!less(k, j)) break;
+            exch(k, j);
+            k = j;
+        }
+    }
+
+    private boolean less(int i, int j) {
+        return pq[i].compareTo(pq[j]) < 0;
+    }
+
+    private void exch(int i, int j) {
+        Key t = pq[i];
+        pq[i] = pq[j];
+        pq[j] = t;
+    }
+}
